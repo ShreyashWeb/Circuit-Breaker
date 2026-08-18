ace1399d16214e1b435baafd7c15c6d820e99aee
sharma-charu <charusharma8711@gmail.com>
2026-08-18 21:35:28 +0530

    update inventory model

--- inventory-service/src/main/java/com/axlero/inventory_service/model/Inventory.java
+++ inventory-service/src/main/java/com/axlero/inventory_service/model/Inventory.java
@@ -1,5 +1,41 @@
 package com.axlero.inventory_service.model;
 
-public class Product {
+public class Inventory {
 
+    private Long id;
+    private Long productId;
+    private int quantity;
+
+    public Inventory() {
+    }
+
+    public Inventory(Long id, Long productId, int quantity) {
+        this.id = id;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
